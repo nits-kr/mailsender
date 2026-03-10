a80e97569b684245347272cbb1ced3e06049b8e3
Nitish Kumar <nits.kr99@gmail.com>
2026-03-10 14:00:39 +0530

    fix(fsock-auto): re-add express-async-handler dependency to fix controller ReferenceErrors

--- src/controllers/fsockAutoController.js
+++ src/controllers/fsockAutoController.js
@@ -4,6 +4,7 @@
  * + checkTestMessageResponse.php + Stats.php + Webhook.php
  */
 
+const asyncHandler = require("express-async-handler");
 const path = require("path");
 const fs = require("fs");
 
